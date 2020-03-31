39b8bbebc76b3bfe0c9efad1153812d1ae6dd3aa
Amit Gupta <amit.gupta@caroobi.com>
2020-03-31 19:01:29 +0530

    UPDATED - Main console file to except inputs form file and stdin

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import argparse
 import fileinput
-import sys
+
+from parking_lot.parking_manager import ParkingManager
 
 
 class Console:
     """A class to perform incoming console requests."""
+
+    def __init__(self):
+        """Create a new parking manager instance."""
+        self.manager = ParkingManager()
 
     def run(self):
         """Run the console command to perform the requests."""
@@ -21,10 +26,18 @@ class Console:
         """Perform the incoming request."""
         command, args = self._parse_request(request)
 
-        if request == 'exit':
+        if command == 'exit':
             raise KeyboardInterrupt
 
-        print(f'{command=} {args=}')
+        if hasattr(self.manager, command):
+            try:
+                output = getattr(self.manager, command)(*args)
+            except Exception as e:
+                output = str(e)
+        else:
+            output = f'Given command [{command}] does not exists'
+
+        print(self._prepare_output(output))
 
     def _parse_request(self, request):
         """Parses the given request in 'command' and 'arguments'."""
@@ -32,9 +45,16 @@ class Console:
 
         return [tokens[0], tokens[1:]]
 
+    def _prepare_output(self, output):
+        """Prepares the output in printable format."""
+        if isinstance(output, list):
+            return ''.join(['{:12}{:19}{}\n'.format(*row) for row in output]).rstrip()
+
+        return output
+
 
 if __name__ == '__main__':
     try:
         Console().run()
-    except KeyboardInterrupt as e:
-        print(e, file=sys.stderr)
+    except KeyboardInterrupt:
+        pass
